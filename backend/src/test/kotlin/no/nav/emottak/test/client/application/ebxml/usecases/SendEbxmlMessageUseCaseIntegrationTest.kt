90f7ccd5481ceababa5cb27cc144bedaf5437e3f
Ivan Skodje <ivanskodje@protonmail.com>
2025-02-03 13:10:37 +0100

    chore: ktlint

--- backend/src/test/kotlin/no/nav/emottak/test/client/application/ebxml/usecases/SendEbxmlMessageUseCaseIntegrationTest.kt
+++ backend/src/test/kotlin/no/nav/emottak/test/client/application/ebxml/usecases/SendEbxmlMessageUseCaseIntegrationTest.kt
@@ -8,9 +8,9 @@ import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
-import java.util.Base64
 import kotlinx.serialization.json.Json
 import no.nav.emottak.test.client.domain.EbxmlResult
+import java.util.Base64
 
 // Integration tests for manual testing
 class SendEbxmlMessageUseCaseIntegrationTest : FunSpec({
