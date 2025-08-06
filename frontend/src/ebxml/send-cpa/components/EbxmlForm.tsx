6929a391f9e605f0e97b5fa4100242d43c3416a8
Ivan Skodje <ivanskodje@protonmail.com>
2025-08-06 13:56:01 +0200

    chore: added xml formatter

--- frontend/src/ebxml/send-cpa/components/EbxmlForm.tsx
+++ frontend/src/ebxml/send-cpa/components/EbxmlForm.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import {Input} from "@/components/ui/input";
-import {Label} from "@/components/ui/label";
-import {Alert} from "@/components/ui/alert";
-import React, {useRef, useState} from "react";
-import {Loader2, RefreshCw} from "lucide-react";
-import {EbxmlResult, sendEbxmlRequest} from "@/ebxml/send-cpa/actions/send-request";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Alert } from "@/components/ui/alert";
+import xmlFormatter from "xml-formatter";
+import React, { useRef, useState } from "react";
+import { Loader2, RefreshCw } from "lucide-react";
+import { EbxmlResult, sendEbxmlRequest } from "@/ebxml/send-cpa/actions/send-request";
 import CodeMirrorWithDelay from "@/ebxml/send-cpa/components/CodeMirror/CodeMirrorWithDelay";
 import CpaTemplateSelector from "@/ebxml/send-cpa/components/CpaTemplateSelector/CpaTemplateSelector";
-import {
-  frikortEgenandelForesporselRequest
-} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/frikort-egenandelforesporsel-request";
-import {Button} from "@/components/ui/button";
+import { frikortEgenandelForesporselRequest } from "@/ebxml/send-cpa/cpa-templates/cpa-requests/frikort-egenandelforesporsel-request";
+import { Button } from "@/components/ui/button";
 import CodeMirror from "@uiw/react-codemirror";
-import {xml} from "@codemirror/lang-xml";
-import {githubLight} from "@uiw/codemirror-theme-github";
-import {generateKibanaURLFromConversationId} from "@/lib/generate-kibana-url";
-import {v4 as uuidv4} from "uuid";
-import {Checkbox} from "@/components/ui/checkbox";
+import { xml } from "@codemirror/lang-xml";
+import { githubLight } from "@uiw/codemirror-theme-github";
+import { generateKibanaURLFromConversationId } from "@/lib/generate-kibana-url";
+import { v4 as uuidv4 } from "uuid";
+import { Checkbox } from "@/components/ui/checkbox";
 
 export default function EbxmlForm() {
   const [formData, setFormData] = useState(frikortEgenandelForesporselRequest);
@@ -41,15 +40,15 @@ export default function EbxmlForm() {
       toRole: "",
       service: "",
       action: "",
-      ebxmlPayload: {base64Content: ""},
-      signPayload: false
+      ebxmlPayload: { base64Content: "" },
+      signPayload: false,
     });
     setError(null);
     setResponse(null);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
@@ -59,7 +58,7 @@ export default function EbxmlForm() {
   const handlePayloadChange = (value: string) => {
     setFormData((prev) => ({
       ...prev,
-      ebxmlPayload: {base64Content: btoa(value)},
+      ebxmlPayload: { base64Content: btoa(value) },
     }));
   };
 
@@ -90,50 +89,34 @@ export default function EbxmlForm() {
     } finally {
       setLoading(false);
       setTimeout(() => {
-        responseRef.current?.scrollIntoView({behavior: "smooth", block: "start"});
+        responseRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
       }, 100);
     }
   };
 
   const logsLink = generateKibanaURLFromConversationId(lastUsedConversationId);
 
+  const xmlFormatterOptions = { indentation: "  ", lineSeparator: "\n" };
   const decodeResponse = (value: string) => {
-    return value.split(/--------=/)
-      .flatMap(c => c.split(/\r?\n\r?\n/))
-      .map(c => {
+    return value
+      .split(/--------=/)
+      .flatMap((chunk) => chunk.split(/\r?\n\r?\n/))
+      .map((section) => {
         try {
-          let line = c.trim()
-          if (line.length === 0) return c
-          const xml = atob(line)
-          return prettifyXml(xml)
-        } catch (t) {
-          return c
+          const line = section.trim();
+          if (line.length === 0) return section;
+          const decodedXml = atob(line);
+          return xmlFormatter(decodedXml, xmlFormatterOptions);
+        } catch {
+          try {
+            return xmlFormatter(section, xmlFormatterOptions);
+          } catch {
+            return section;
+          }
         }
-      }).filter(e => e.length !== 0)
-      .join("\n\n")
-  };
-
-  const prettifyXml = function (sourceXml: string) {
-    let xmlDoc = new DOMParser().parseFromString(sourceXml, 'application/xml');
-    let xsltDoc = new DOMParser().parseFromString([
-      // describes how we want to modify the XML - indent everything
-      '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
-      '  <xsl:strip-space elements="*"/>',
-      '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
-      '    <xsl:value-of select="normalize-space(.)"/>',
-      '  </xsl:template>',
-      '  <xsl:template match="node()|@*">',
-      '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
-      '  </xsl:template>',
-      '  <xsl:output indent="yes"/>',
-      '</xsl:stylesheet>',
-    ].join('\n'), 'application/xml');
-
-    let xsltProcessor = new XSLTProcessor();
-    xsltProcessor.importStylesheet(xsltDoc);
-    let resultDoc = xsltProcessor.transformToDocument(xmlDoc);
-    let resultXml = new XMLSerializer().serializeToString(resultDoc);
-    return resultXml;
+      })
+      .filter((e) => e.length !== 0)
+      .join("\n\n");
   };
 
   return (
@@ -141,7 +124,7 @@ export default function EbxmlForm() {
       <div className="flex justify-between">
         <div>
           <Label>CPA Template:</Label>
-          <CpaTemplateSelector selectedTemplate={formData} onTemplateChange={setFormData}/>
+          <CpaTemplateSelector selectedTemplate={formData} onTemplateChange={setFormData} />
         </div>
         <Button type="button" variant="outline" className="bg-red-100" onClick={handleClear}>
           Clear Form
@@ -156,14 +139,14 @@ export default function EbxmlForm() {
               id="conversationId"
               name="conversationId"
               value={formData.conversationId}
-              onChange={(e) => setFormData({...formData, conversationId: e.target.value})}
+              onChange={(e) => setFormData({ ...formData, conversationId: e.target.value })}
             />
             <Checkbox
               id="autoReloadConversationId"
               checked={autoReloadConversationId}
               onCheckedChange={(checked) => setAutoReloadConversationId(!!checked)}
             />
-            <RefreshCw className="w-5 h-5 text-gray-500 cursor-pointer"/>
+            <RefreshCw className="w-5 h-5 text-gray-500 cursor-pointer" />
           </div>
         </div>
       </div>
@@ -183,14 +166,14 @@ export default function EbxmlForm() {
               checked={autoReloadMessageId}
               onCheckedChange={(checked) => setAutoReloadMessageId(!!checked)}
             />
-            <RefreshCw className="w-5 h-5 text-gray-500 cursor-pointer"/>
+            <RefreshCw className="w-5 h-5 text-gray-500 cursor-pointer" />
           </div>
         </div>
       </div>
 
       <div>
         <Label htmlFor="cpaId">CPA ID</Label>
-        <Input id="cpaId" name="cpaId" value={formData.cpaId} onChange={handleChange}/>
+        <Input id="cpaId" name="cpaId" value={formData.cpaId} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="fromPartyId">From Party ID</Label>
@@ -203,23 +186,23 @@ export default function EbxmlForm() {
       </div>
       <div>
         <Label htmlFor="fromRole">From Role</Label>
-        <Input id="fromRole" name="fromRole" value={formData.fromRole} onChange={handleChange}/>
+        <Input id="fromRole" name="fromRole" value={formData.fromRole} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="toPartyId">To Party ID</Label>
-        <Input id="toPartyId" name="toPartyId" value={formData.toPartyId} onChange={handleChange}/>
+        <Input id="toPartyId" name="toPartyId" value={formData.toPartyId} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="toRole">Role</Label>
-        <Input id="toRole" name="toRole" value={formData.toRole} onChange={handleChange}/>
+        <Input id="toRole" name="toRole" value={formData.toRole} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="service">Service</Label>
-        <Input id="service" name="service" value={formData.service} onChange={handleChange}/>
+        <Input id="service" name="service" value={formData.service} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="action">Action</Label>
-        <Input id="action" name="action" value={formData.action} onChange={handleChange}/>
+        <Input id="action" name="action" value={formData.action} onChange={handleChange} />
       </div>
       <div>
         <Label htmlFor="payload">Payload</Label>
@@ -229,7 +212,7 @@ export default function EbxmlForm() {
         />
       </div>
       <Button type="submit" disabled={loading} variant={"outline"} className="bg-green-100">
-        {loading && <Loader2 className="animate-spin w-5 h-5"/>}
+        {loading && <Loader2 className="animate-spin w-5 h-5" />}
         {loading ? "Sending..." : "Send Request"}
       </Button>
 
@@ -247,12 +230,16 @@ export default function EbxmlForm() {
               </div>
               <Label>DECODED:</Label>
               <Alert variant="default">
-                <CodeMirror readOnly value={decodeResponse(response)} extensions={[xml()]}
-                            theme={githubLight}/>
+                <CodeMirror
+                  readOnly
+                  value={decodeResponse(response)}
+                  extensions={[xml()]}
+                  theme={githubLight}
+                />
               </Alert>
               RAW
               <Alert variant="default">
-                <CodeMirror readOnly value={response} extensions={[xml()]} theme={githubLight}/>
+                <CodeMirror readOnly value={response} extensions={[xml()]} theme={githubLight} />
               </Alert>
             </div>
           </>
