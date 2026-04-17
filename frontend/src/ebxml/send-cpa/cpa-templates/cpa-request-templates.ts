9f1bc2e0dbea4ff8087b8b733ce7d939c78d5cb1
Thomas Burnett <7643677+thburnett@users.noreply.github.com>
2026-04-17 13:05:12 +0200

    refactor: remove Pasientliste request templates and associated integr… (#40)
    * refactor: remove Pasientliste request templates and associated integration test
    
    * refactor: remove Pasientliste request templates and associated integration test
    
    ---------
    
    Co-authored-by: Thomas Burnett <thburnett@users.noreply.github.com>

--- frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
+++ frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
@@ -1,9 +1,5 @@
 import { frikortEgenandelForesporselRequest } from "@/ebxml/send-cpa/cpa-templates/cpa-requests/frikort-egenandelforesporsel-request";
-import { pasientlisteForesporselHentPasientlisteRequest } from "@/ebxml/send-cpa/cpa-templates/cpa-requests/pasientlisteforesporsel-hentpasientliste-request";
 import { EbxmlRequest } from "@/ebxml/send-cpa/types";
-import {
-  pasientlisteForesporselHentPasientlisteWithHelseIdRequest
-} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/pasientlisteforesporsel-hentpasientliste-request-with-helseid";
 import {trekkopplysningInnmelding} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/trekkopplysning-innmelding";
 
 export type CpaRequestTemplate = {
@@ -17,16 +13,6 @@ export const cpaRequestTemplates: CpaRequestTemplate[] = [
     type: "Frikort",
     name: "EgenandelForesporsel",
     value: frikortEgenandelForesporselRequest,
-  },
-  {
-    type: "Pasientliste",
-    name: "HentPasientliste",
-    value: pasientlisteForesporselHentPasientlisteRequest,
-  },
-  {
-    type: "Pasientliste",
-    name: "HentPasientliste med HelseID",
-    value: pasientlisteForesporselHentPasientlisteWithHelseIdRequest,
   },
   {
     type: "Trekkopplysning",
