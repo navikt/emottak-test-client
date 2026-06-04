a6cb0f72c5d53c1a333a98469ae3d47c2f5105ca
Aage Kramarics <Aage.Kramarics@nav.no>
2026-06-04 09:34:05 +0200

    La til template for sykmelding, og dummy for legemelding

--- frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
+++ frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
@@ -1,6 +1,8 @@
 import { frikortEgenandelForesporselRequest } from "@/ebxml/send-cpa/cpa-templates/cpa-requests/frikort-egenandelforesporsel-request";
 import { EbxmlRequest } from "@/ebxml/send-cpa/types";
 import {trekkopplysningInnmelding} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/trekkopplysning-innmelding";
+import {sykmeldingRegistrering} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/sykmelding-registrering";
+import {legemeldingLegeerklaring} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/legemelding-legeerklaring";
 
 export type CpaRequestTemplate = {
   type: string;
@@ -18,5 +20,15 @@ export const cpaRequestTemplates: CpaRequestTemplate[] = [
     type: "Trekkopplysning",
     name: "Trekkopplysning Innmelding",
     value: trekkopplysningInnmelding,
+  },
+  {
+    type: "Sykmelding",
+    name: "Sykmelding Registrering",
+    value: sykmeldingRegistrering,
+  },
+  {
+    type: "Legemelding",
+    name: "Legemelding Legeerklaring",
+    value: legemeldingLegeerklaring,
   },
 ];
