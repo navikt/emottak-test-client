b30454d7fc9a1c3b259952845c5bdc590dc23be7
Aage Kramarics <Aage.Kramarics@nav.no>
2026-06-26 08:12:26 +0200

    Template for DialogmoteInnkalling, MoteRespons

--- frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
+++ frontend/src/ebxml/send-cpa/cpa-templates/cpa-request-templates.ts
@@ -3,6 +3,7 @@ import { EbxmlRequest } from "@/ebxml/send-cpa/types";
 import {trekkopplysningInnmelding} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/trekkopplysning-innmelding";
 import {sykmeldingRegistrering} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/sykmelding-registrering";
 import {legemeldingLegeerklaring} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/legemelding-legeerklaring";
+import {dialogmoteInnkallingMoteRespons} from "@/ebxml/send-cpa/cpa-templates/cpa-requests/dialogmoteinnkalling-moterespons";
 import {
     frikortMengdeEgenandelForesporselRequest
 } from "@/ebxml/send-cpa/cpa-templates/cpa-requests/frikortmengde-egenandelforesporsel-request";
@@ -38,5 +39,10 @@ export const cpaRequestTemplates: CpaRequestTemplate[] = [
     type: "Legemelding",
     name: "Legemelding Legeerklaring",
     value: legemeldingLegeerklaring,
+  },
+  {
+    type: "DialogmoteInnkalling",
+    name: "DialogmoteInnkalling MoteRespons",
+    value: dialogmoteInnkallingMoteRespons,
   },
 ];
